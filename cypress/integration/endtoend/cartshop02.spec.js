7e46d77b7dc7aca058cf85126b33bc264974d395
carlos.dorival.esquivel@gmail.com <carlos.dorival.esquivel@gmail.com>
2022-06-01 19:37:58 -0500

    test 02

--- cypress/integration/endtoend/cartshop02.spec.js
+++ cypress/integration/endtoend/cartshop02.spec.js
@@ -29,7 +29,26 @@ describe('shopping cart', ()=>{
         cy.get('.confirm').should('be.visible').click();
     })
 
-   
+    it('buy laptop',()=>{
+        //buy phone
+        cy.xpath('/html/body/div[5]/div/div[1]/div/a[3]').click();
+        cy.get(':nth-child(5) > .card > .card-block > .card-title > .hrefch').click();
+        cy.get('.col-sm-12 > .btn', {timeout:4000}).should('be.visible');
+        cy.get('.col-sm-12 > .btn').click();
+        cy.get('#cartur').click();
+        cy.get('.col-lg-1 > .btn').click();
+        cy.wait(1000);
+        //place order
+        cy.get('#name').type('name test');
+        cy.get('#country').type('country test01');
+        cy.get('#city').type('city test01');
+        cy.get('#card').type('123456789');
+        cy.get('#month').type('5');
+        cy.get('#year').type('2022');
+        cy.get('#orderModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary').click();
+        cy.wait(1000);
+        cy.get('.confirm').should('be.visible').click();
+    })
 
 
 })
